4531cfd7862e18ccf496502c7923183d967c18b7
adamjoha <adamtorkkeli@gmail.com>
2017-12-23 20:54:45 +0100

    Nu är spelplanen fylld med rutor där varje ruta från början är svart.

--- untitled/src/PlayingField.java
+++ untitled/src/PlayingField.java
@@ -5,13 +5,34 @@ import java.awt.*;
  * Created by AdamDaBaows on 2017-12-23.
  */
 public class PlayingField extends JPanel {
-    public PlayingField(int height, int width){
+    //dela in spelplanen i rutor(squares).
+    private Square[][] squares;
+    //rutorna har alltid en sidolängd på 10 pixlar.
+    public final int square_sidelength = 10;
 
+    public PlayingField(int height, int width){
+        setupSquares(height, width);
     }
+
+    private void setupSquares(int height, int width){
+        //initialisera arrayen av rutor.
+        squares = new Square[height/square_sidelength][width/square_sidelength];
+        //fyll den med rutor som täcker hela spelplanen.
+        for(int i = 0; i < height; i += square_sidelength){
+            for(int j = 0; j < width; j += square_sidelength){
+                squares[i/square_sidelength][j/square_sidelength] = new Square(i, j);
+            }
+        }
+    }
+
 
     @Override
     public void paintComponent(Graphics g){
-        g.setColor(Color.black);
-        g.fillRect(0, 0, 10, 10);
+        for(Square[] squares_: squares){
+            for(Square square: squares_){
+                g.setColor(square.getColor());
+                g.fillRect(square.getX_positions(), square.getY_position(), square_sidelength, square_sidelength);
+            }
+        }
     }
 }
--- untitled/src/Square.java
+++ untitled/src/Square.java
@@ -1,38 +1,40 @@
+import java.awt.*;
+
 /**
- * Created by AdamDaBaows on 2017-12-21.
+ * enkel klass för en fyrkant. Varje fyrkant är från början svart.
  */
 public class Square {
     private int x_positions;
     private int y_position;
-    private int height;
-    private int width;
+    private Color color;
 
     public Square(int x_positions, int y_position) {
         this.x_positions = x_positions;
         this.y_position = y_position;
+        color = Color.black;
     }
 
     public int getX_positions() {
         return x_positions;
     }
 
-    public int getY_position() {
-        return y_position;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
     public void setX_positions(int x_positions) {
         this.x_positions = x_positions;
     }
 
+    public int getY_position() {
+        return y_position;
+    }
+
     public void setY_position(int y_position) {
         this.y_position = y_position;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
